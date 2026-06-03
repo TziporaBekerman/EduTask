59cea19142b8a01655077306fac58e1b369f53e9
MyName <me@example.com>
2026-06-03 10:59:55 +0300

    add server structure

--- server/database/db.js
+++ server/database/db.js
@@ -1,21 +1,16 @@
-const express = require('express');
-const db = require('./config/db'); // ייבוא קובץ החיבור
+const mysql = require('mysql2');
+require('dotenv').config(); // טוען את המשתנים מקובץ ה-.env (יודע לעלות תיקייה אחת למעלה לבד)
 
-const app = express();
-require('dotenv').config();
+// יצירת מאגר החיבורים (Connection Pool) ל-SQL
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-// בדיקת חיבור לדיבייטס בהפעלת השרת
-async function testConnection() {
-    try {
-        // מריץ שאילתה פשוטה רק כדי לראות שיש תגובה
-        const [rows] = await db.query('SELECT 1 + 1 AS result');
-        console.log('✅ החיבור ל-SQL הצליח בהצלחה!');
-    } catch (err) {
-        console.error('❌ שגיאה בהתחברות ל-SQL:', err.message);
-    }
-}
-
-testConnection();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = pool.promise();
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const db = require('./config/db'); // ייבוא קובץ החיבור
+const cors = require("cors");
+
+const app = express();
+require('dotenv').config();
+
+async function testConnection() {
+    try {
+        const [rows] = await db.query('SELECT 1 + 1 AS result');
+        console.log('✅ החיבור ל-SQL הצליח בהצלחה!');
+    } catch (err) {
+        console.error('❌ שגיאה בהתחברות ל-SQL:', err.message);
+    }
+}//למחוק לפני ההגשה?
+testConnection();
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
+app.use(express.json());
+
+app.use("/users", require("./routes/userRoutes"));
+app.use("/Assignments", require("./routes/assignmentsRoutes"));
+app.use("/Submissions", require("./routes/submissionsRoutes")); 
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
