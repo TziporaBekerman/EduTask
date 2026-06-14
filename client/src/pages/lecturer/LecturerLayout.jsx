d2d0c9d541819a603e5a46253f37b17d3e25553e
sarabrie <sarabr0278@gmail.com>
2026-06-14 18:09:12 +0300

    lecturer

---
+++ client/src/pages/lecturer/LecturerLayout.jsx
@@ -0,0 +1,12 @@
+import Layout from "../../common/Layout";
+
+const links = [
+  { to: "/lecturer/home",        label: "דף הבית" },
+  { to: "/lecturer/submissions", label: "הגשות וציונים" },
+  { to: "/lecturer/profile",     label: "פרופיל" },
+  { to: "/lecturer/assignments", label: "ניהול מטלות" },
+];
+
+export default function LecturerLayout() {
+  return <Layout links={links} />;
+}
